64ef32cc742a82f70241efcddf487dcefbe31d34
Alex Pereira Maranhão <alexmaranhao@saneago.com.br>
2020-07-13 05:42:56 -0300

    configura resource server

--- src/main/java/br/com/mpx/core/config/security/ResourceServerConfig.java
+++ src/main/java/br/com/mpx/core/config/security/ResourceServerConfig.java
@@ -1,7 +1,13 @@
 package br.com.mpx.core.config.security;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
@@ -11,9 +17,15 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.authenticated()
 			.and()
-			.oauth2ResourceServer().opaqueToken();
-			
-			
+			.oauth2ResourceServer().jwt();				
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecorer() {
+	  
+	  SecretKey secret = new SecretKeySpec("hrljebfasnfb23rlkjfansdjfnasdmnfjahlkjhewhjfasdlkjfhjh2k3jhkwjhlakjfha".getBytes(), "HmacSHA256");
+	  
+	  return NimbusJwtDecoder.withSecretKey(secret).build();
 	}
 	
 	
